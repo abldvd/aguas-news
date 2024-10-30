74e2b4c400919f95a21a624d9016519c07e3d79f
abaresag <abldvd.aresag@gmail.com>
2024-10-30 08:47:01 +0000

    añadido script para la ejecucion automatica (en bash)

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Nombre de la imagen
+IMAGE_NAME="aguas-news"
+
+# Función para construir la imagen
+build_image() {
+    echo "Construyendo la imagen Docker..."
+    docker build -t $IMAGE_NAME .
+}
+
+# Comprobar si el primer argumento es "build"
+if [ "$1" == "build" ]; then
+    build_image
+else
+    # Comprobar si la imagen existe
+    if ! docker image inspect $IMAGE_NAME > /dev/null 2>&1; then
+        echo "La imagen '$IMAGE_NAME' no existe. Iniciando construcción..."
+        build_image
+    fi
+fi
+
+# Ejecutar el contenedor con el volumen montado
+echo "Obteniendo noticias......"
+docker run --rm -v "$(pwd)/output":/usr/src/app/output $IMAGE_NAME
